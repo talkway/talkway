5e2f5068ba2779993c78b74a88e9820669269e30
Garbrand van der Molen <garbrand@gmail.com>
2016-03-17 22:34:22 +0100

    Grabbed current location from browser. Resolves issue #3

---
+++ code/locator.js
@@ -0,0 +1,40 @@
+// # Algorithms
+function checkGeolocationSupport() {
+	// check for Geolocation support
+	if (navigator.geolocation) {
+	  console.log('Geolocation is supported!');
+	}
+	else {
+	  alert('Geolocation is not supported for this Browser/OS version yet.');
+	}
+}
+
+function positionHandler ( position ) {
+	// Handle position information
+	document.getElementById( 'Latitude' ).innerHTML  = position.coords.latitude;
+	document.getElementById( 'Longitude' ).innerHTML = position.coords.longitude;
+	
+	console.log( "latitude: %s \t longitude: %s", position.coords.latitude, position.coords.longitude );
+}
+
+function errorHandler ( error ) {
+	alert('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from locaton provider)
+    //   3: timed out
+}
+
+
+
+
+// # Bootstrap
+// check location on load
+window.onload = function() {
+  checkGeolocationSupport();
+  navigator.geolocation.getCurrentPosition( positionHandler, errorHandler );
+};
+
+// monitor location -- fires whenever position changes
+navigator.geolocation.watchPosition( positionHandler );
