e069fb2fe0f229824f1db1e2e0d1dc0bd7b65be9
Garbrand van der Molen <garbrand@gmail.com>
2016-03-24 12:18:18 +0100

    Added type repo

---
+++ types/message.js
@@ -0,0 +1,8 @@
+var User = require( './user.js' );
+
+var Message = {
+	id:       Number,
+	user:     User,
+	data:     String,
+	metadata: Object
+}
---
+++ types/user.js
@@ -0,0 +1,4 @@
+var User = {
+	id:   Number,
+	name: String
+}
