79ee5f45c6c4dde022749aa65c97ce23fd52434f
Garbrand van der Molen <garbrand@gmail.com>
2016-09-29 19:37:27 +0200

    Added license db

--- licenses/db/test/db.js
+++ licenses/db/test/db.js
@@ -9,15 +9,17 @@ let db = DB( { db: data } );
 console.assert( Object.keys( data ).length === 3, 'Dataset starts with 3 items' );
 
 // Save
-let save = db.save( 'XX-TJ-80', "Mini Cooper S" );
+const stub = { 'XX-TJ-80': { description: "Mini Cooper S" } };
+const key  = Object.keys( stub )[0];
+
+let save = db.save( key, stub[ key ] );
 console.assert( Object.keys( data ).length === 4, 'Dataset now is 4 items' );
-console.assert( save === "Mini Cooper S", 'db.save should return the data' );
+console.assert( save === stub[ key ], 'db.save should return the data' );
 
 // Get
-let get = db.get( 'XX-TJ-80' );
-console.assert( get === "Mini Cooper S" );
+console.assert( db.get( key ) === stub[ key ], 'Able to retrieve data from the store' );
 
 // Delete
 let del = db.delete( 'XX-TJ-80' );
-console.assert( del );
+console.assert( del, 'Delete an item' );
 console.assert( Object.keys( data ).length === 3 );
